10be300a31066412f23b3741484a8d5f554a1d16
Bastien Guerry <bastien.guerry@data.gouv.fr>
2020-03-25 09:51:04 +0100

    Trying .txt extension

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 rm -fr resources/public/js/*
 clj -A:js
-cp src/cljs/choices/custom.cljs docs/exemples/clojure.clj
+cp src/cljs/choices/custom.cljs docs/exemples/clojure.txt
 cp resources/public/js/choices.js docs/js/
 echo "static js generated"
 
