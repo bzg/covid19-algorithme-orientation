90c3c45a683e59005c107794ae234cc0a7b946d1
Denis Merigoux <denis.merigoux@gmail.com>
2020-03-25 18:25:03 +0100

    More documentation

--- z3/covid.py
+++ z3/covid.py
@@ -2,39 +2,72 @@
 
 from z3 import *
 
+# D'abord, il faut déclarer créer une instance du solveur Z3
 s = Solver()
 
+# On déclare toutes les entrées du problèmes, booléennes ou entières
 age = Int("age")
 fievre = Bool("fievre")
 toux = Bool("toux")
 mal_de_gorge = Bool("mal_de_gorge")
 anosmie = Bool("anosmie")
 diarrhee = Bool("diarrhee")
-
 facteurs_pronostiques = Int("facteurs_pronostiques")
-s.add(facteurs_pronostiques >= 0)
 facteurs_gravite_mineur = Int("facteurs_gravite_mineur")
+facteurs_gravite_majeur = Int("facteurs_gravite_majeur")
+
+# Le solveur Z3 fonctionne en raisonnant sur un ensemble de contraintes portant
+# sur les variables du problèmes. On ajoute des contraintes avec s.add()
+# Ici, les contraintes sur les entrées.
+s.add(age >= 0)
+s.add(facteurs_pronostiques >= 0)
 s.add(facteurs_gravite_mineur >= 0)
 s.add(facteurs_gravite_mineur <= 2)
-facteurs_gravite_majeur = Int("facteurs_gravite_majeur")
 s.add(facteurs_gravite_majeur >= 0)
 s.add(facteurs_gravite_majeur <= 2)
 
-# Contraintes définition facteurs gravité
+# Contraintes sur la définition des facteurs gravité mineurs : la fièvre en
+# fait partie.
 s.add(Implies(fievre, facteurs_gravite_mineur > 0))
 s.add(Implies(facteurs_gravite_mineur == 2, fievre))
 
+# Liste des variables du problème
 variables = [age, fievre, toux, mal_de_gorge, anosmie, diarrhee,
              facteurs_pronostiques, facteurs_gravite_majeur, facteurs_gravite_mineur]
 
+# L'algotithme définit 10 cas de terminaison que l'on stockera dans cette liste
+# "done"
 done = [False for i in range(1, 10)]
-implies_done_acc = False
+
+# La fonction suivante nous permet d'encoder comme une contrainte le fait
+# de déclarer une condition de terminaison de l'algorithme.
+# Ici "cond" est la condition à laquelle on veut que l'algorithme termine, et
+# "number" le numéro du cas de terminaison.
+
 
 def implies_done(cond, number):
+    # L'algorithme est séquentiel : lorsque l'on atteint une condition de
+    # terminaison, on arrête de l'exécuter. Or Z3 n'est pas séquentiel, les
+    # contraintes ne fonctionnent pas comme ça. On a donc besoin ici d'un
+    # accumulateur des conditions de terminaisons déjà rencontrées afin
+    # d'émuler ce comportement séquentiel pour Z3.
     global implies_done_acc
+    # On termine dans ce cas si la condition est remplie et que l'on est pas
+    # dans un des cas de terminaison précédent.
     cond_acced = And(cond, Not(implies_done_acc))
+    # On peut définit plusieurs conditions qui mènent au même cas de terminaison,
+    # il est donc nécessaire de faire un "ou" logique avec le contenu de la
+    # case de la liste.
     done[number - 1] = Or(done[number - 1], cond_acced)
+    # On met à jour l'accumulateur
     implies_done_acc = Or(cond, implies_done_acc)
+
+
+# L'accumulateur est initialisé à False car c'est une série de ou logiques.
+implies_done_acc = False
+
+# ## Encodage du pseudo-code dans Z3
+# Le code Python ci-dessous est plutôt lisible, étant donné "implies_done"
 
 #  SI moins de 15 ans => FIN1
 implies_done(age <= 15, 1)
@@ -83,6 +116,7 @@ implies_done(And(cond5, cond7, facteurs_gravite_mineur > 1), 4)
 implies_done(facteurs_gravite_majeur > 0, 5)
 #
 #  SI toux OU mal de gorge OU anosmie ET pas de fièvre
+# (ici doute sur la manière de parser le dernier ET)
 cond8_alt1 = Or(toux, mal_de_gorge, And(anosmie, Not(fievre)))
 cond8_alt2 = And(Or(toux, mal_de_gorge, anosmie), Not(fievre))
 cond8 = cond8_alt1
@@ -95,7 +129,10 @@ implies_done(
 #  SI NI toux NI mal de gorge NI anosmie NI fièvre => FIN9
 implies_done(And(Not(toux), Not(mal_de_gorge), Not(anosmie), Not(fievre)), 9)
 
-
+# Maintenant que l'algorithme est encodé dans Z3, on peut essayer de prouver
+# les théorèmes. Cette fonction wrappe la fonction "check" de Z3 avec une
+# interface qui affiche la valeur des variables du contre-exemple si le théorème
+# que l'on essaye de prouver est faux.
 def check_and_print():
     c = s.check()
     if c == sat:
@@ -113,18 +150,32 @@ def check_and_print():
         print("Théorème prouvé !\n")
 
 
+print("Théorème: l'algorithme termine dans tous les cas")
 s.push()
+# Comment encoder ce théorème dans Z3 ? On se rappelle que "done" contient
+# la liste de tous les cas de terminaisons, exprimés comme des formules logiques
+# en fonction des variables d'entrée.
+# Pour savoir si l'on termine dans tous les cas, il suffit de faire un "ou"
+# logique de toutes les conditions de terminaison. En effet, si l'on passe
+# la négation de ce "ou" à Z3, alors le solveur va essayer de trouver une
+# combinaison des variables d'entrées telle que l'on ne soit dans aucun des
+# cas de terminaison prévu. Si le solveur ne trouve pas de telle combinaison,*
+# alors le théorème est prouvé.
 cond = Or(done)
 s.add(Not(cond))
-print("Théorème: l'algorithme termine dans tous les cas")
 check_and_print()
 s.pop()
 
+print("Théorème: les conditions de sortie sont mutuellement exclusives")
 s.push()
-for j in range(0,9):
-    for i in range(0,j):
+# Ici, il s'agit de vérifier qu'il n'existe pas de manière de quitter l'algorithme
+# avec plusieurs cas de terminaison déclenchés. Cette propriété est triviale
+# pour un pseudo-code séquentiel (quand on termine, on arrête d'exécute),
+# donc ici ce théorème sert surtout à montrer que la manière dont on a émulé
+# le comportement séquentiel à Z3 est bien cohérente.
+for j in range(0, 9):
+    for i in range(0, j):
         cond = Not(And(done[i], done[j]))
         s.add(Not(cond))
-print("Théorème: les conditions de sortie sont mutuellement exclusives")
 check_and_print()
 s.pop()
