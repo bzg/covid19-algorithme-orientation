a8495c11482d7b43b0ac99f58ab0f039360e3915
Bastien Guerry <bastien.guerry@data.gouv.fr>
2020-03-26 19:14:16 +0100

    Small formatting fixes

--- src/clj/choices/vars.clj
+++ src/clj/choices/vars.clj
@@ -29,7 +29,7 @@
 
 (def repl-fr-meta (inline-yaml-resource "website/fr/repl-meta.yml"))
 
-(def exemple-de-reponse ";; Vous pouvez modifier les valeurs ci-dessous
+(def exemple-de-reponse ";; Vous pouvez modifier les valeurs ci-dessous.
 {:fievre                   1
 :diarrhees                0
 :toux                     1
@@ -43,7 +43,7 @@
 :facteurs-gravite-majeurs 0}")
 
 (def resultat ";; Vous pouvez modifier cette fonction pour la tester.
-(defn resultat [reponse]
+  (defn resultat [reponse]
   (let [reponse
         ;; Calcul du facteur âge, de l'IMC et de son impact sur les
         ;; facteurs de pronostique défavorable
@@ -62,7 +62,8 @@
     (cond
       ;; Branche 1
       (= moins-de-15-ans 1)
-      (println \"   Branche: 1 (moins de 15 ans)\")
+      (do (println \"Branche: 1 (moins de 15 ans)\")
+          (println \"FIN1: Prenez contact avec votre médecin généraliste au moindre doute. Cette application n’est pour l’instant pas adaptée aux personnes de moins de 15 ans. En cas d’urgence, appeler le 15.\"))
       ;; Branche 2
       (or (and (> fievre 0) (= toux 0))
           (and (> toux 0) (> mal-de-gorge 0))
@@ -70,45 +71,45 @@
           (and (> fievre 0) (> diarrhees 0)))
       (do (println \"   Branche: 2 (fièvre ou autres symptômes)\")
           (cond (>= facteurs-gravite-majeurs 1)
-                (println \"FIN5\")
+                (println \"FIN5: Appel du 15.\")
                 (= facteurs-pronostique 0)
                 (if (= facteurs-gravite-mineurs 0)
                   (if (= plus-de-50-ans 0)
-                    (println \"FIN2\")
-                    (println \"FIN3\"))
-                  (println \"FIN3\"))
+                    (println \"FIN2: Nous vous conseillons de rester à votre domicile et de contacter votre médecin en cas d’apparition de nouveaux symptômes. Vous pourrez aussi utiliser à nouveau l’application pour réévaluer vos symptômes.\")
+                    (println \"FIN3: Téléconsultation ou médecin généraliste ou visite à domicile (SOS médecins). Appelez le 15 si une gêne respiratoire ou des difficultés importantes pour s’alimenter ou boire pendant plus de 24h apparaissent.\"))
+                  (println \"FIN3: Téléconsultation ou médecin généraliste ou visite à domicile (SOS médecins). Appelez le 15 si une gêne respiratoire ou des difficultés importantes pour s’alimenter ou boire pendant plus de 24h apparaissent.\"))
                 (>= facteurs-pronostique 1)
                 (cond (< facteurs-gravite-mineurs 2)
-                      (println \"FIN3\")
+                      (println \"FIN3: Téléconsultation ou médecin généraliste ou visite à domicile (SOS médecins). Appelez le 15 si une gêne respiratoire ou des difficultés importantes pour s’alimenter ou boire pendant plus de 24h apparaissent.\")
                       (>= facteurs-gravite-mineurs 2)
-                      (println \"FIN4\"))))
+                      (println \"FIN4: Téléconsultation ou médecin généraliste ou visite à domicile (SOS médecins). Si pas possible de le joindre ou téléCS : faite le 15.\"))))
       ;; Branche 3
       (>= facteurs-gravite-majeurs 1)
-      (do (println \"   Branche: 3 (Un facteur majeur de gravité)\")
-          (println \"FIN5\"))
+      (do (println \"Branche: 3 (Un facteur majeur de gravité)\")
+          (println \"FIN5: Appel du 15.\"))
       ;; Branche 4
       (and (> fievre 0) (> toux 0))
-      (do (println \"   Branche: 4 (Fièvre et toux)\")
+      (do (println \"Branche: 4 (Fièvre et toux)\")
           (if (= facteurs-pronostique 0)
             (when (> facteurs-gravite-mineurs 0)
-              (println \"FIN6\"))
+              (println \"FIN6: Téléconsultation ou médecin généraliste ou visite à domicile (SOS médecins).\"))
             (when (>= facteurs-pronostique 1)
               (cond (>= facteurs-gravite-mineurs 0)
-                    (println \"FIN6\")
+                    (println \"FIN6: Téléconsultation ou médecin généraliste ou visite à domicile (SOS médecins).\")
                     (>= facteurs-gravite-mineurs 2)
-                    (println \"FIN4\")))))
+                    (println \"FIN4: Téléconsultation ou médecin généraliste ou visite à domicile (SOS médecins). Si pas possible de le joindre ou téléCS : faite le 15.\")))))
       ;; Branche 5
       (and (= fievre 0)
            (or (> toux 0) (> mal-de-gorge 0) (> anosmie 0)))
-      (do (println \"   Branche: 5 (Pas de fièvre et un autre symptôme\")
+      (do (println \"Branche: 5 (Pas de fièvre et un autre symptôme\")
           (cond (= facteurs-gravite-mineurs 0)
-                (println \"FIN7\")
+                (println \"FIN7: Votre situation ne relève probablement pas du Covid-19. Consultez votre médecin au moindre doute. Si de nouveaux symptomes apparaissent, refaites le test.\")
                 (or (>= facteurs-gravite-mineurs 1) (>= facteurs-pronostique 1))
-                (println \"FIN8\")))
+                (println \"FIN8: Votre situation ne relève probablement pas du Covid-19. Un avis médical est recommandé. Au moindre doute, appelez le 15.\")))
       ;; Branche 6
       (and (= fievre 0) (= toux 0) (= mal-de-gorge 0) (= anosmie 0))
-      (do (println \"   Branche: 6 (Pas de symptôme)\")
-          (println \"FIN9\")))))")
+      (do (println \"Branche: 6 (Pas de symptôme)\")
+          (println \"FIN9: Votre situation ne relève probablement pas du Covid-19. N’hésitez pas à contacter votre médecin en cas de doute. Vous pouvez refaire le test en cas de nouveau symptôme pour réévaluer la situation. Pour toute information concernant le Covid-19, composer le 0 800 130 000.\")))))")
 
 (def repl-fr-contents
   [:section
@@ -116,7 +117,7 @@
     [:br]
     [:h1.title "Code de l'indice de masse corporelle"]
     [:pre.language-klipse
-     ";; La taille est ici indiquée en centimètre
+     ";; La taille est ici indiquée en centimètre.
 (defn compute-imc [poids taille] (/ poids (Math/pow (/ taille 100.0) 2)))"]]
 
    [:div.container
