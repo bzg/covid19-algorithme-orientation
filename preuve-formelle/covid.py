01ee9381d52ee4e9f8b523a3882fdca9f0f88f65
Denis Merigoux <denis.merigoux@gmail.com>
2020-03-28 18:16:02 +0100

    Added new algorithm

--- preuve-formelle/covid.py
+++ preuve-formelle/covid.py
@@ -9,7 +9,7 @@ s = Solver()
 age = Int("age")
 fievre = Bool("fievre")
 toux = Bool("toux")
-mal_de_gorge = Bool("mal_de_gorge")
+douleurs = Bool("douleurs")
 anosmie = Bool("anosmie")
 diarrhee = Bool("diarrhee")
 facteurs_pronostiques = Int("facteurs_pronostiques")
@@ -32,12 +32,13 @@ s.add(Implies(fievre, facteurs_gravite_mineur > 0))
 s.add(Implies(facteurs_gravite_mineur == 2, fievre))
 
 # Liste des variables du problème
-variables = [age, fievre, toux, mal_de_gorge, anosmie, diarrhee,
+variables = [age, fievre, toux, douleurs, anosmie, diarrhee,
              facteurs_pronostiques, facteurs_gravite_majeur, facteurs_gravite_mineur]
 
 # L'algotithme définit 10 cas de terminaison que l'on stockera dans cette liste
 # "done"
-done = [False for i in range(1, 10)]
+done_new = [BoolVal(False) for i in range(1, 10)]
+done_old = [BoolVal(False) for i in range(1, 10)]
 
 # La fonction suivante nous permet d'encoder comme une contrainte le fait
 # de déclarer une condition de terminaison de l'algorithme.
@@ -45,13 +46,12 @@ done = [False for i in range(1, 10)]
 # "number" le numéro du cas de terminaison.
 
 
-def implies_done(cond, number):
+def implies_done_base(cond, number, done, implies_done_acc):
     # L'algorithme est séquentiel : lorsque l'on atteint une condition de
     # terminaison, on arrête de l'exécuter. Or Z3 n'est pas séquentiel, les
     # contraintes ne fonctionnent pas comme ça. On a donc besoin ici d'un
     # accumulateur des conditions de terminaisons déjà rencontrées afin
     # d'émuler ce comportement séquentiel pour Z3.
-    global implies_done_acc
     # On termine dans ce cas si la condition est remplie et que l'on est pas
     # dans un des cas de terminaison précédent.
     cond_acced = And(cond, Not(implies_done_acc))
@@ -60,92 +60,150 @@ def implies_done(cond, number):
     # case de la liste.
     done[number - 1] = Or(done[number - 1], cond_acced)
     # On met à jour l'accumulateur
-    implies_done_acc = Or(cond, implies_done_acc)
+    return Or(cond, implies_done_acc)
 
 
 # L'accumulateur est initialisé à False car c'est une série de ou logiques.
-implies_done_acc = False
+implies_done_acc_new = BoolVal(False)
+implies_done_acc_old = BoolVal(False)
+
+def implies_done_new(cond, number):
+    global implies_done_acc_new
+    implies_done_acc_new = implies_done_base(cond, number, done_new, implies_done_acc_new)
+
+def implies_done_old(cond, number):
+    global implies_done_acc_old
+    implies_done_acc_old = implies_done_base(cond, number, done_old, implies_done_acc_old)
 
 # ## Encodage du pseudo-code dans Z3
 # Le code Python ci-dessous est plutôt lisible, étant donné "implies_done"
 
+## VERSION MISE À JOUR DE L'ALGO
+
+#  SI < 15 ans => FIN1
+implies_done_new(age <= 15, 1)
+#
+#  SI >=1 facteur de gravité majeur => FIN5
+implies_done_new(facteurs_gravite_majeur >= 1, 5)
+#
+#  SI fièvre ET toux
+cond1 = And(fievre, toux)
+#     SI >=1 facteur pronostique et >=2 facteurs de gravité mineurs => FIN4
+implies_done_new(And(cond1, facteurs_pronostiques >= 1, facteurs_gravite_mineur >= 2), 4)
+#     SINON => FIN6
+implies_done_new(And(cond1, Not(And(facteurs_pronostiques >= 1, facteurs_gravite_mineur >= 2))), 6)
+#
+#  SI fièvre OU diarrhée OU (toux ET douleurs) OU (toux ET anosmie)
+cond2 = Or(fievre, diarrhee, And(toux, douleurs), And(toux, anosmie))
+#     SI >=1 facteur pronostique
+cond3 = (facteurs_pronostiques >= 1)
+#        SI >=2 facteurs de gravité mineurs => FIN4
+implies_done_new(And(cond2, cond3, facteurs_gravite_mineur >= 2), 4)
+#        SINON => FIN3
+implies_done_new(And(cond2, cond3, facteurs_gravite_mineur < 2), 3)
+#     SINON
+#        SI >= 50 ans ou >=1 facteur de gravité mineur => FIN3
+cond4 = Or(age >= 50, facteurs_gravite_mineur >= 1)
+implies_done_new(And(cond2, Not(cond3), cond4), 3)
+#        SINON => FIN2
+implies_done_new(And(cond2, Not(cond3), Not(cond4)), 2)
+#
+#  SI pas de fièvre ET (toux OU douleurs OU anosmie)
+cond5 = And(Not(fievre), Or(toux, douleurs, anosmie))
+#     SI >=1 facteur de gravité mineur OU >=1 facteur pronostique => FIN8
+cond6 = Or(facteurs_gravite_mineur >= 1, facteurs_pronostiques >= 1)
+implies_done_new(And(cond5, cond6), 8)
+#     SINON => FIN7
+implies_done_new(And(cond5, Not(cond6)), 7)
+#
+#  SI NI toux NI douleurs NI anosmie => FIN9
+implies_done_new(And(Not(toux), Not(douleurs), Not(anosmie)), 9)
+
+# OLD VERSION OF THE ALGORITHM
+
 #  SI moins de 15 ans => FIN1
-implies_done(age <= 15, 1)
+implies_done_old(age <= 15, 1)
 #
 #  SI (fièvre ET PAS toux) OU (toux ET mal de gorge) OU (toux ET anosmie) OU (fièvre ET diarrhée)
-cond1 = Or(And(fievre, Not(toux)), And(toux, mal_de_gorge), And(
+cond1 = Or(And(fievre, Not(toux)), And(toux, douleurs), And(
     toux, anosmie), And(fievre, diarrhee))
 #     SI 0 facteur pronostique
 cond2 = (facteurs_pronostiques == 0)
 #        SI 0 facteur de gravité mineur
 cond3 = (facteurs_gravite_mineur == 0)
 #           SI moins de 50 ans => FIN2
-implies_done(And(cond1, cond2, cond3, age < 50), 2)
+implies_done_old(And(cond1, cond2, cond3, age < 50), 2)
 #           SINON => FIN3
-implies_done(And(cond1, cond2, cond3, Not(age > 50)), 3)
+implies_done_old(And(cond1, cond2, cond3, Not(age > 50)), 3)
 #        SINON => FIN3
-implies_done(And(cond1, cond2), 3)
+implies_done_old(And(cond1, cond2), 3)
 #
 #     SI 1 OU plus facteurs pronostique
 cond4 = (facteurs_pronostiques > 0)
 #        SI 0 OU 1 facteur de gravité mineur => FIN3
-implies_done(And(cond1, cond4, Or(facteurs_gravite_mineur ==
+implies_done_old(And(cond1, cond4, Or(facteurs_gravite_mineur ==
                                   0, facteurs_gravite_mineur == 1)), 3)
 #        SI au moins 2 facteurs de gravité mineurs => FIN4
-implies_done(And(cond1, cond4, facteurs_gravite_mineur > 1), 4)
+implies_done_old(And(cond1, cond4, facteurs_gravite_mineur > 1), 4)
 #
 #     SI un facteur de gravité majeur => FIN5
-implies_done(And(cond1, facteurs_gravite_majeur > 0), 5)
+implies_done_old(And(cond1, facteurs_gravite_majeur > 0), 5)
 #
 #  SI fièvre ET toux
 cond5 = And(fievre, toux)
 #     SI 0 facteur pronostique
 cond6 = (facteurs_pronostiques == 0)
 #        SI 0 OU 1 facteur de gravité mineurs => FIN6
-implies_done(And(cond5, cond6, Or(facteurs_gravite_mineur ==
+implies_done_old(And(cond5, cond6, Or(facteurs_gravite_mineur ==
                                   0, facteurs_gravite_mineur == 1)), 6)
 #     SI 1 OU plus facteur pronostique
 cond7 = (facteurs_pronostiques > 0)
 #        SI 0 OU 1 facteur de gravité mineurs => FIN6
-implies_done(And(cond5, cond7, Or(facteurs_gravite_mineur ==
+implies_done_old(And(cond5, cond7, Or(facteurs_gravite_mineur ==
                                   0, facteurs_gravite_mineur == 1)), 6)
 #        SI au moins 2 facteurs de gravité mineurs => FIN4
-implies_done(And(cond5, cond7, facteurs_gravite_mineur > 1), 4)
+implies_done_old(And(cond5, cond7, facteurs_gravite_mineur > 1), 4)
 #
 #  SI 1 OU plus facteur de gravité majeur => FIN5
-implies_done(facteurs_gravite_majeur > 0, 5)
+implies_done_old(facteurs_gravite_majeur > 0, 5)
 #
 #  SI toux OU mal de gorge OU anosmie ET pas de fièvre
 # (ici doute sur la manière de parser le dernier ET)
-cond8_alt1 = Or(toux, mal_de_gorge, And(anosmie, Not(fievre)))
-cond8_alt2 = And(Or(toux, mal_de_gorge, anosmie), Not(fievre))
+cond8_alt1 = Or(toux, douleurs, And(anosmie, Not(fievre)))
+cond8_alt2 = And(Or(toux, douleurs, anosmie), Not(fievre))
 cond8 = cond8_alt1
 #     SI au moins un facteur de gravité mineur OU un facteur pronostique => FIN8
-implies_done(
+implies_done_old(
     And(cond8, Or(facteurs_gravite_mineur > 0, facteurs_pronostiques > 0)), 8)
 #     SI 0 facteur de gravité mineur => FIN7
-implies_done(And(cond8, facteurs_gravite_mineur == 0), 7)
+implies_done_old(And(cond8, facteurs_gravite_mineur == 0), 7)
 #
 #  SI NI toux NI mal de gorge NI anosmie NI fièvre => FIN9
-implies_done(And(Not(toux), Not(mal_de_gorge), Not(anosmie), Not(fievre)), 9)
+implies_done_old(And(Not(toux), Not(douleurs), Not(anosmie), Not(fievre)), 9)
 
 # Maintenant que l'algorithme est encodé dans Z3, on peut essayer de prouver
 # les théorèmes. Cette fonction wrappe la fonction "check" de Z3 avec une
 # interface qui affiche la valeur des variables du contre-exemple si le théorème
 # que l'on essaye de prouver est faux.
-def check_and_print():
+def check_and_print(to_print):
     c = s.check()
     if c == sat:
-        print("Exemple :")
+        print("Contre-exemple :")
         print("  Valeur des variables :")
         variables_evaluated = ["    {}: {}".format(
             v, s.model().evaluate(v, model_completion=True)) for v in variables]
         print(*variables_evaluated, sep="\n")
         print("  Sorties activées :")
-        done_evaluated = [("    FIN{}".format(
-            i + 1), s.model().evaluate(d, model_completion=True)) for (i, d) in enumerate(done)]
-        done_evaluated = [a for (a, b) in done_evaluated if b]
-        print(*done_evaluated, sep="\n")
+        if to_print == "new" or to_print == "both":
+            done_evaluated = [("    FIN{} (nouveau)".format(
+            i + 1), s.model().evaluate(d, model_completion=True)) for (i, d) in enumerate(done_new)]
+            done_evaluated = [a for (a, b) in done_evaluated if b]
+            print(*done_evaluated, sep="\n")
+        if to_print == "old" or to_print == "both":
+            done_evaluated = [("    FIN{} (ancien)".format(
+            i + 1), s.model().evaluate(d, model_completion=True)) for (i, d) in enumerate(done_old)]
+            done_evaluated = [a for (a, b) in done_evaluated if b]
+            print(*done_evaluated, sep="\n")
     elif c == unsat:
         print("Théorème prouvé !\n")
 
@@ -161,16 +219,25 @@ s.push()
 # combinaison des variables d'entrées telle que l'on ne soit dans aucun des
 # cas de terminaison prévu. Si le solveur ne trouve pas de telle combinaison,*
 # alors le théorème est prouvé.
-cond = Or(done)
+cond = Or(done_new)
 s.add(Not(cond))
-check_and_print()
+check_and_print(done_new)
 s.pop()
 
-print("Théorème: chaque sortie est atteignable")
+# print("Théorème: chaque sortie est atteignable")
+# for i in range(0, 9):
+#     print("Atteindre la sortie FIN{} ?".format(i + 1))
+#     s.push()
+#     cond = done_new[i]
+#     s.add(cond)
+#     check_and_print("new")
+#     s.pop()
+
+print("Théorème: vieux et nouveau algorithme sont les mêmes")
 for i in range(0, 9):
-    print("Atteindre la sortie FIN{} ?".format(i + 1))
+    print("Sortie FIN{} ?".format(i + 1))
     s.push()
-    cond = done[i]
+    cond = Xor(done_old[i], done_new[i])
     s.add(cond)
-    check_and_print()
+    check_and_print("both")
     s.pop()
