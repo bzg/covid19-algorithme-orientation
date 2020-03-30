9f6f29586a51de842c962c081de19f4dadef89ae
Bastien <bzg@gnu.org>
2020-03-30 07:54:51 +0200

    Update config.yml, build.sh and generate js/html output

--- build.sh
+++ build.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-if [[ $1 == "all" ]]; then
+# Compile js for dev.cljs.edn
+if [[ $1 == "dev" ]]; then
    rm -fr resources/public/js/*
    clj -A:js
-   cp src/cljs/choices/custom.cljs docs/exemples/clojure.txt
    cp resources/public/js/choices.js docs/js/
    echo "static js generated"
 fi
+
+# Compile js only for dev-nav.
+if [[ $1 == "nav" ]]; then
+   rm -fr resources/public/js/*
+   clj -A:js-nav
+   cp resources/public/js/choices-nav.js docs/js/
+   echo "static js generated"
+fi
+
+# Update implementation example
+cp src/cljs/choices/custom.cljs docs/exemples/clojure.txt
 
 # Build json files
 clj -m choices.json
 
 # Build *.html
 clj -m choices.build
-
