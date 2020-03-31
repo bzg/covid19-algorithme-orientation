8e701718edd1fe536919c6e070ef28bff7ba8438
Bastien <bzg@gnu.org>
2020-03-31 11:55:26 +0200

    Update generated js and json

--- src/clj/choices/json.clj
+++ src/clj/choices/json.clj
@@ -16,7 +16,31 @@
        node))
    data))
 
-(defn -main []
+(defn algo-with-data-collection []
+  (let [parsed-config   (yaml/parse-string (slurp "config-nav.yml"))
+        tree            (map
+                         (fn [branch]
+                           (if (re-matches #"^(1\.1|^1\.2)$" (:node branch))
+                             (merge branch {:choices "Saisie utilisateur"})
+                             branch))
+                         (remove-deep (:tree parsed-config) [:color]))
+        score-variables (remove-deep (:score-variables parsed-config) [:display])
+        conditional-score-outputs1
+        (remove-deep (:conditional-score-outputs parsed-config)
+                     [:priority] #"condition-\d+")]
+    (spit "docs/json/variables-nav.json"
+          (json/generate-string
+           {:variables score-variables}))
+    (spit "docs/json/questions-nav.json"
+          (json/generate-string
+           {:questions (map
+                        (fn [n] (select-keys n [:node :text :choices]))
+                        (remove #(= (:home-page %) true) tree))}))
+    (spit "docs/json/conclusions-nav.json"
+          (json/generate-string
+           {:conclusions conditional-score-outputs1}))))
+
+(defn algo-without-data-collection []
   (let [parsed-config   (yaml/parse-string (slurp "config.yml"))
         tree            (map
                          (fn [branch]
@@ -41,6 +65,9 @@
            {:conclusions conditional-score-outputs1}))
     (spit "docs/json/conclusions-with-priority.json"
           (json/generate-string
-           {:conclusions-with-priority (:conditional-score-outputs parsed-config)}))
-    (println "Files docs/json/*.json generated")))
+           {:conclusions-with-priority (:conditional-score-outputs parsed-config)}))))
 
+(defn -main []
+  (algo-with-data-collection)
+  (algo-without-data-collection)
+  (println "Files docs/json/*.json generated"))
